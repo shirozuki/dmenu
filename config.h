eec1cb12b097c084979a9a23e727c5ab7f93f8fe
Artur Dobrowolski <artur.dobrowolski@startmail.com>
2024-06-29 22:29:58 +0200

    PATCH: https://tools.suckless.org/dmenu/patches/xresources-alt/

--- config.h
+++ config.h
@@ -1,22 +1,32 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
+/* Size of the window border */
+static unsigned int border_width = 0;
+
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int centered = 0;                    /* -c option; centers dmenu on screen */
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const int user_bh = 0;               /* add an defined amount of pixels to the bar height */
+static char font[] = "monospace:size=10";
 static const char *fonts[] = {
-	"monospace:size=10"
+	font,
+	"monospace:size=10",
 };
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-static const char *colors[SchemeLast][2] = {
+
+static char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static char normfgcolor[] = "#bbbbbb";
+static char normbgcolor[] = "#222222";
+static char selfgcolor[]  = "#eeeeee";
+static char selbgcolor[]  = "#005577";
+static char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeNorm] = { normfgcolor, normbgcolor },
+	[SchemeSel]  = { selfgcolor,  selbgcolor  },
 	[SchemeSelHighlight] = { "#ffc978", "#005577" },
 	[SchemeNormHighlight] = { "#ffc978", "#222222" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeOut]  = { "#000000",   "#00ffff" },
 	[SchemeOutHighlight] = { "#ffc978", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
@@ -28,5 +38,15 @@ static unsigned int lines      = 0;
  */
 static const char worddelimiters[] = " ";
 
-/* Size of the window border */
-static unsigned int border_width = 0;
+/*
+ * Xresources preferences to load at startup
+ */
+ResourcePref resources[] = {
+	{ "font",        	STRING, &font },
+	{ "normfgcolor", 	STRING, &normfgcolor },
+	{ "normbgcolor", 	STRING, &normbgcolor },
+	{ "selfgcolor",  	STRING, &selfgcolor },
+	{ "selbgcolor",  	STRING, &selbgcolor },
+	{ "prompt",      	STRING, &prompt },
+	{ "border_width",	INTEGER, &border_width },
+};
